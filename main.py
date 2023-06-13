f8254ec058315fd660346243d3411149d13c0124
Wiktor Pytlewski <wiktorpytlewski@gmail.com>
2023-06-13 23:13:14 +0200

    przygotowanie do testów i rysowania tabelek

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-import json
 import os
-import glob
-from Classes import Dataset, State, Action, Slot, Defence
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from Classes import Dataset, Slot, Defence
 from Enviroment import Environment
 from Qlearning import QLearning
 from SARSA import SARSA
 from RLexperiment import RLExperiment
 
+
+# Przygotowywanie danych o slotach
 with open('data/slots/time_slots.json', 'r') as f:
     slots_data = json.load(f)
+slots = [Slot(index, time) for index, time in slots_data.items()]
 
+# Przygotowywanie danych o obronach
 with open('data/obrony/obrony.json', 'r') as f:
     defences_data = json.load(f)
-
-# with open('data/people/przewodniczacy.json', 'r') as f:
-#     chairmans = json.load(f)
-
-# with open('data/people/workers.json', 'r') as f:
-#     members = json.load(f)
-
-
-folder_path = 'data/examples'
-file_names = os.listdir(folder_path)
-sorted_file_names = sorted(file_names, key=lambda x: int(x.split('.')[0]))
-examples_paths_list = [os.path.join(folder_path, file_name) for file_name in sorted_file_names]
-
 defences = [Defence(defence_data["student"], defence_data["promotor"], defence_data["recenzent"]) for defence_data in defences_data]
-slots = [Slot(index, time) for index, time in slots_data.items()]
+
+# Przygotowywanie przykładów
+examples_folder_path = 'data/examples'
+file_names = os.listdir(examples_folder_path)
+sorted_file_names = sorted(file_names, key=lambda x: int(x.split('.')[0]))
+examples_paths_list = [os.path.join(examples_folder_path, file_name) for file_name in sorted_file_names]
 
 for example_path in examples_paths_list:
     dataset = Dataset(example_path)
@@ -42,43 +39,69 @@ for example_path in examples_paths_list:
     q_experiment = RLExperiment(q_env, q_agent, num_episodes=1000, max_steps_per_episode=30)
     q_experiment.run()
 
-    # print(len(q_experiment.schedule))
+    sorted_data = sorted(q_experiment.get_schedule_json(), key=lambda x: x['numer slotu'])
+    with open(os.path.join(example_path, 'qlearning_results.json'), 'w', encoding='utf-8') as outfile:
+        json.dump(sorted_data, outfile, indent=1)
 
-    if len(q_experiment.schedule) == 30:
+    # SARSA
+    sarsa_env = Environment(defences, slots, chairman, members, availability, chairman_avail)
+    sarsa_agent = SARSA()
 
-        sorted_data = sorted(q_experiment.get_schedule_json(), key=lambda x: x['numer slotu'])
-        with open('test.json', 'w', encoding='utf-8') as outfile:
-            json.dump(sorted_data, outfile, indent=1)
-        exit()
+    sarsa_experiment = RLExperiment(sarsa_env, sarsa_agent, num_episodes=10000, max_steps_per_episode=30)
+    sarsa_experiment.run()
 
-    # # SARSA
-    # sarsa_env = Environment(defences, slots, chairman, members, availability, chairman_avail)
-    # sarsa_agent = SARSA()
-
-    # sarsa_experiment = RLExperiment(sarsa_env, sarsa_agent, num_episodes=10000, max_steps_per_episode=30)
-    # sarsa_experiment.run()
-
-    # if len(sarsa_experiment.schedule) == 30:
-    #     print(example_path)
+    sorted_data = sorted(sarsa_experiment.get_schedule_json(), key=lambda x: x['numer slotu'])
+    with open(os.path.join(example_path, 'sarsa_results.json'), 'w', encoding='utf-8') as outfile:
+        json.dump(sorted_data, outfile, indent=1)
 
 
-# print(examples_paths_list[0])
-# a = Dataset(examples_paths_list[0])
-# print(a.get_workers_avail())
-# print(a.get_chairman_avail())
+exit()
+# load workload data
+with open("data/slots/time_slots.json") as f:
+    slot_time = json.load(f)
 
-# a = Defence(1, 2, 3)
-# b = Defence(1, 2, 4)
-# print(a == b)
+# load timetable data
+with open("test.json") as f:
+    schedules = json.load(f)
 
-# a = State(1, 2, 3)
-# b = State(1, 2, 4)
-# print(a == b)
+# convert to dataframe
+df = pd.DataFrame(schedules)
 
-# a = Action(1, 2)
-# b = Action(1, 4)
-# print(a == b)
+# map slot number to time slot
+df['numer slotu'] = df['numer slotu'].astype(str).map(slot_time)
 
-# a = Slot(1, 2)
-# b = Slot(1, 4)
-# print(a == b)
+df['Obrona'] = df['student']+'\n'+df['promotor']
+
+ee = pd.DataFrame(df['numer slotu'].str.split('-', expand=True))
+ee['Time'] = ee[1] + '-' + ee[2]
+ee = ee.drop([1, 2], axis=1)
+ee = ee.rename(columns={0: 'Day'})
+# print(ee)
+# extract day and time to new columns
+df[['Day', 'Time']] = ee
+
+# pivot to create timetable
+timetable = df.pivot(index='Time', columns='Day', values='Obrona')
+
+desired_order = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek']
+
+# Select columns in desired order
+timetable = timetable.reindex(columns=desired_order)
+
+fig, ax = plt.subplots(1, 1)
+
+# Remove axis
+ax.axis('off')
+
+# Add a table
+table = ax.table(cellText=timetable.values,
+                 colLabels=timetable.columns,
+                 rowLabels=timetable.index,
+                 cellLoc = 'center',
+                 loc='center')
+
+table.auto_set_font_size(False)
+table.set_fontsize(10)
+table.scale(1, 2) # you can set the scale according to your specific needs
+
+plt.show()
